8e059ff603b7bad476a1552c271fed39ce9da30d
Lucas Godoy <godoy.lucas.e@gmail.com>
2018-01-21 17:48:11 +1300

    Configured uServices to be discovered by using eureka

--- src/main/java/com/devit/promomanager/PromoManagerApplication.java
+++ src/main/java/com/devit/promomanager/PromoManagerApplication.java
@@ -3,6 +3,7 @@ package com.devit.promomanager;
 import org.dozer.DozerBeanMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,6 +14,7 @@ import java.util.List;
 @SpringBootApplication
 @EnableSwagger2
 @ComponentScan(basePackages = {"io.swagger", "com.devit"})
+@EnableDiscoveryClient
 public class PromoManagerApplication {
 
 	public static void main(String[] args) {
